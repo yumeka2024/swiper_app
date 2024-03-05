e0afcb5113f581f21d530a0763078eeb078c79c0
yumeka2024 <154503350+yumeka2024@users.noreply.github.com>
2024-03-05 04:04:18 +0000

    [Add]swiper

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,10 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'jquery'
 import "script.js"
+import Swiper from 'swiper/swiper-bundle.js';  //この行を追記
+import 'swiper/swiper-bundle.css';
 
 Rails.start()
 Turbolinks.start()
